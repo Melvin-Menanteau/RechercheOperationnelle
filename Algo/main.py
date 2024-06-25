22d4f412f5ddc71b06c6ffb21d53fa728a010d60
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2024-06-25 09:52:48 +0200

    feat (main): Réduire l'utilisation des variables globales

--- Algo/main.py
+++ Algo/main.py
@@ -12,19 +12,19 @@ from gurobipy import *
 # Limite de temps pour la résolution du problème (en secondes)
 TIME_LIMIT = 300
 # Nombre de camions
-NB_TRUCKS = 3
+NB_TRUCKS = 2
 # Nombre de villes
-NB_VERTICES = 20
+NB_VERTICES = 23
 # Ville de départ (doit être < NB_VERTICES)
 START_CITY = round(NB_VERTICES / 2)
 # Probabilité d'avoir une arête entre deux sommets (chemin entre deux villes)
 PROBABILITY_EDGE = 0.4
 # Poids (distance) minimum d'une arête
-MIN_WEIGHT = 10
+MIN_WEIGHT = 5
 # Poids (distance) maximum d'une arête
-MAX_WEIGHT = 500
+MAX_WEIGHT = 50
 # Valeur infinie pour représenter une connexion impossible
-INF = 1e100
+INF = 1e10
 # Couleurs pour les arêtes parcourues par les camions
 COLORS = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "gray"]
 
@@ -45,13 +45,14 @@ def create_graph(nb_vertices):
 
 # Définir des poids aléatoires pour les arêtes du graphe
 def set_random_weights(graph):
-    matrix = [(0, 0) for _ in range(NB_VERTICES)]
+    nb_vertices = graph.vcount()
+    matrix = [(0, 0) for _ in range(nb_vertices)]
 
-    for i in range(NB_VERTICES):
+    for i in range(nb_vertices):
         # matrix[i] = (np.random.randint(MIN_WEIGHT, MAX_WEIGHT), np.random.randint(MIN_WEIGHT, MAX_WEIGHT))
         matrix[i] = (MIN_WEIGHT * i, MIN_WEIGHT * i)
 
-    print(matrix)
+    # print(matrix)
 
     for edge in graph.es:
         # edge["weight"] = np.random.randint(MIN_WEIGHT, MAX_WEIGHT)
@@ -73,58 +74,56 @@ def get_adjacency_matrix(graph):
 
 def solve(matrix):
     # Créer le problème PuLP
+    nb_vertices = len(matrix)
+    start_city = round(nb_vertices / 2)
     prob = pulp.LpProblem("VRP", pulp.LpMinimize)
 
     # ------------------------------------------------- #
     # Variables de décision
     # ------------------------------------------------- #
     # Indique si l'arête (i, j) est empruntée par le camion k
-    c = pulp.LpVariable.dicts("c", [(i, j, k) for i in range(NB_VERTICES) for j in range(NB_VERTICES) for k in range(NB_TRUCKS)], lowBound=0, upBound=1, cat=pulp.LpBinary)
+    c = pulp.LpVariable.dicts("c", [(i, j, k) for i in range(nb_vertices) for j in range(nb_vertices) for k in range(NB_TRUCKS)], lowBound=0, upBound=1, cat=pulp.LpBinary)
     # Indique la position du camion k
-    u = pulp.LpVariable.dicts("u", (i for i in range(1, NB_VERTICES)), lowBound=1, upBound=NB_VERTICES-1, cat=pulp.LpInteger)
+    u = pulp.LpVariable.dicts("u", (i for i in range(1, nb_vertices)), lowBound=1, upBound=nb_vertices-1, cat=pulp.LpInteger)
 
     # Fonction objectif
-    prob += pulp.lpSum(matrix[i][j] * c[(i, j, k)] for i in range(NB_VERTICES) for j in range(NB_VERTICES) for k in range(NB_TRUCKS))
+    prob += pulp.lpSum(matrix[i][j] * c[(i, j, k)] for i in range(nb_vertices) for j in range(nb_vertices) for k in range(NB_TRUCKS))
 
     # ------------------------------------------------- #
     # Contraintes
     # ------------------------------------------------- #
     # Chaque ville doit être visitée au moins une fois
-    for i in range(NB_VERTICES):
-        prob += pulp.lpSum(c[(i, j, k)] for j in range(NB_VERTICES) for k in range(NB_TRUCKS)) >= 1
+    for i in range(nb_vertices):
+        prob += pulp.lpSum(c[(i, j, k)] for j in range(nb_vertices) for k in range(NB_TRUCKS)) >= 1
 
     # L'ensemble des chemins parcourus forment un cycle
     for k in range(NB_TRUCKS):
-        for i in range(1, NB_VERTICES):
-            prob += pulp.lpSum(c[(i, j, k)] for j in range(NB_VERTICES)) - pulp.lpSum(c[(j, i, k)] for j in range(NB_VERTICES)) == 0
+        for i in range(1, nb_vertices):
+            prob += pulp.lpSum(c[(i, j, k)] for j in range(nb_vertices)) - pulp.lpSum(c[(j, i, k)] for j in range(nb_vertices)) == 0
 
     # Tous les camions doivent partir de la ville START_CITY
     for k in range(NB_TRUCKS):
-        prob += pulp.lpSum(c[START_CITY, j, k] for j in [x for x in range(NB_VERTICES) if x != START_CITY]) == 1
+        prob += pulp.lpSum(c[start_city, j, k] for j in [x for x in range(nb_vertices) if x != start_city]) == 1
 
     # Tous les camions doivent revenir à la ville START_CITY
     for k in range(NB_TRUCKS):
-        prob += pulp.lpSum(c[i, START_CITY, k] for i in [x for x in range(NB_VERTICES) if x != START_CITY]) == 1
+        prob += pulp.lpSum(c[i, start_city, k] for i in [x for x in range(nb_vertices) if x != start_city]) == 1
 
     # Ne pas boucler sur la même ville
     for k in range(NB_TRUCKS):
-        prob += pulp.lpSum(c[(i, i, k)] for i in range(NB_VERTICES)) == 0
+        prob += pulp.lpSum(c[(i, i, k)] for i in range(nb_vertices)) == 0
 
-    for i in range(1, NB_VERTICES):
-        for j in range(1, NB_VERTICES):
+    # Empêcher les sous-tours (contrainte de Miller-Tucker-Zemlin)
+    for i in range(1, nb_vertices):
+        for j in range(1, nb_vertices):
             if i != j:
                 for k in range(NB_TRUCKS):
-                    prob += u[i] - u[j] + (NB_VERTICES - 1) * c[(i, j, k)] <= NB_VERTICES - 2
-
-    # La répartition entre les camions doit être équitable
-    # for i in range(1, NB_VERTICES):
-    #     prob += pulp.lpSum(c[(i, j, k)] for j in range(NB_VERTICES) for k in range(NB_TRUCKS)) == 1
+                    prob += u[i] - u[j] + (nb_vertices - 1) * c[(i, j, k)] <= nb_vertices - 2
 
     # Paramètres du solveur pour limiter le temps de résolution (en secondes)
+    prob.solve(pulp.GUROBI(Cuts=1, Presolve=1, Heuristics=0, OutputFlag=0, msg=0, TimeLimit=TIME_LIMIT, logPath="gurobi.log"))
     # solver = pulp.PULP_CBC_CMD(timeLimit=TIME_LIMIT)
-    # solver = pulp.getSolver("GUROBI_CMD", timeLimit=TIME_LIMIT)
-
-    prob.solve(pulp.GUROBI(Cuts=0, Presolve=0, Heuristics=0, OutputFlag=1, msg=1, TimeLimit=TIME_LIMIT))
+    # prob.solver(solver)
 
     # Afficher les résultats
     print("Status:", pulp.LpStatus[prob.status])
@@ -134,21 +133,16 @@ def solve(matrix):
         if v.varValue > 0 and v.name.startswith("c"):
             print(v.name, "=", v.varValue, v)
 
-    return [(i, j, k) for i in range(NB_VERTICES) for j in range(NB_VERTICES) for k in range(NB_TRUCKS) if c[(i, j, k)].varValue > 0]
+    return [(i, j, k) for i in range(nb_vertices) for j in range(nb_vertices) for k in range(NB_TRUCKS) if c[(i, j, k)].varValue > 0]
 
 def main():
     graphe = create_graph(nb_vertices=NB_VERTICES)
     matrice = get_adjacency_matrix(graph=graphe)
 
-    # print(matrice)
-
     solution = solve(matrice)
 
-    print(solution)
-    # solution = None
-
     # Colorier les arêtes parcourues par les camions
-    BASE_COLOR = "#ececec"
+    BASE_COLOR = "#f5f5f5"
 
     for edge in graphe.es:
         try:
